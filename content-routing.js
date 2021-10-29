7f9ca05fa121a8633829bd5de46314e8b019a328
Henrique Dias <hacdias@gmail.com>
2021-10-29 14:40:24 +0200

    feat: finish content routing test without metrics
    License: MIT
    Signed-off-by: Henrique Dias <hacdias@gmail.com>

--- content-routing.js
+++ content-routing.js
@@ -2,6 +2,7 @@ const delay = require('delay')
 const PeerId = require('peer-id')
 const { Multiaddr } = require('multiaddr')
 const { bureaucracy, createNode } = require('./utils')
+const { CID } = require('multiformats/cid')
 
 module.exports = bureaucracy(async (runenv, client, netclient) => {
   let node
@@ -11,6 +12,7 @@ module.exports = bureaucracy(async (runenv, client, netclient) => {
     nodeCreated: 'nodeCreated',
     bootstrap: 'bootstrap',
     connected: 'connected',
+    published: 'published',
     done: 'done'
   }
 
@@ -46,26 +48,61 @@ module.exports = bureaucracy(async (runenv, client, netclient) => {
       multiaddrs = multiaddrs.map(ma => new Multiaddr(ma))
       runenv.recordMessage(`got bootstrap node: ${JSON.stringify(bootstrapNode)}`)
 
-      // Connect to the neighbour only.
-      node.peerStore.addressBook.set(peerId, multiaddrs)
-      await node.dial(peerId)
-      runenv.recordMessage('dialed bootstrap node')
+      if (seq !== 2) {
+        // Connect to the bootstrap node.
+        node.peerStore.addressBook.set(peerId, multiaddrs)
+        await node.dial(peerId)
+        runenv.recordMessage('dialed bootstrap node')
 
-      // The DHT routing tables need a moment to populate
-      await delay(100)
+        // The DHT routing tables need a moment to populate
+        await delay(100)
 
-      // Wait for all peers to have dialed the bootstrap node.
-      await client.signalAndWait(states.connected, runenv.testInstanceCount - 1)
+        // Wait for all peers to have dialed the bootstrap node.
+        await client.signalAndWait(states.connected, runenv.testInstanceCount - 2)
+      }
+
+      const cid = CID.parse('QmTp9VkYvnHyrqKQuFPiuZkiX9gPcqj6x5LJ1rmWuSySnL')
+
+      if (seq === 2) {
+        // Join the network later and find the content.
+        const b = await client.barrier(states.published, 1)
+        await b.wait
+
+        runenv.recordMessage('will join now')
+
+        // Connect to the bootstrap node.
+        node.peerStore.addressBook.set(peerId, multiaddrs)
+        await node.dial(peerId)
+        runenv.recordMessage('dialed bootstrap node')
+
+        // The DHT routing tables need a moment to populate
+        await delay(100)
+
+        runenv.recordMessage('will try findind content')
+
+        for await (const provider of node.contentRouting.findProviders(cid, { timeout: 3000 })) {
+          runenv.recordMessage(`found provider: ${provider.id.toString()}`)
+        }
+      } else if (seq === 3) {
+        // Provide the content.
+
+        await node.contentRouting.provide(cid)
+
+        runenv.recordMessage('providind content')
+
+        // Wait for propagation.
+        await delay(5000)
+
+        await client.signalEntry(states.published)
+      }
     }
 
     // Wait for all the nodes before exiting.
     await client.signalAndWait(states.done, runenv.testInstanceCount)
   } finally {
+    // TODO: node hangs while stopping. Adding a delay reduces the likeliness.
+    await delay(1000)
     runenv.recordMessage('stopping node')
-    if (node) {
-      // TODO: this hangs, leading to a failures due to exceeded deadlines.
-      await node.stop()
-    }
-    runenv.recordMessage('stopping node')
+    if (node) await node.stop()
   }
 })
